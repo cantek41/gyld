4ae0949a51aee0d8a1b9c392bd3404c0134dbb87
can tekin <cantek41@gmail.com>
2023-06-17 19:08:20 +0300

    add bootstrap

--- src/services/BaseService.py
+++ src/services/BaseService.py
@@ -6,11 +6,15 @@ from src.logservice.LoggerService import ILoggerService
 
 class BaseService(metaclass=abc.ABCMeta):
 
-    def __init__(self, data_base: DataBaseContainer, logger: ILoggerService):
+    def __init__(self, data_base: DataBaseContainer, logger: ILoggerService, **kwargs):
         self.data_base_bot = data_base.data_base_bot
         self.data_base_warehouse = data_base.data_base_warehouse
         self.data_base_mining = data_base.data_base_mining
         self.logger = logger
+        self.bootstrap(kwargs)
+
+    def bootstrap(self, **kwargs):
+        pass
 
     def run(self):
         raise NotImplemented
